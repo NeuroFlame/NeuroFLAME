498244ce608539cca7ea83d53d2e2f71007852c5
Ross Kelly <5331533+rssk@users.noreply.github.com>
2025-06-24 13:24:04 -0400

    fix ci build, crossplat

--- scripts/dockerbuild-for-ci.sh
+++ scripts/dockerbuild-for-ci.sh
@@ -1,5 +1,15 @@
 set -e
 
+
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Compute the absolute path to initialize_configs.sh
+INIT_SCRIPT="$SCRIPT_DIR/../configs/initialize_configs.sh"
+
+# Run the script with 'ci' argument
+"$INIT_SCRIPT" ci
+
 docker build -f dockerfiles/Dockerfile-ci -t neuroflame/ci .
 docker build -f dockerfiles/Dockerfile-api -t neuroflame/api .
 docker build -f dockerfiles/Dockerfile-centralFederatedClient -t neuroflame/centralfederatedclient .
