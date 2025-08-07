585104c11b892236792540f9fb7b667894305748
Ross Kelly <5331533+rssk@users.noreply.github.com>
2025-08-07 19:58:29 -0400

    gh action debug

--- docker-compose-ci.sh
+++ docker-compose-ci.sh
@@ -4,6 +4,7 @@ set -e
 # Get the absolute path of the directory this script resides in
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export HOST_PROJECT_DIR="$SCRIPT_DIR"
+echo "Using HOST_PROJECT_DIR: $HOST_PROJECT_DIR"
 
 # Run docker compose using the env var
 docker compose up --exit-code-from ui
