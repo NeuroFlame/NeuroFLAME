0d0bea665b3de8cd906abd4ce7280d897cc5d861
Xiao Tian <kalitopix@gmail.com>
2025-08-19 13:37:55 -0400

    update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
-const neostandard = require('neostandard')
+const neostandard = require('neostandard');
 
 module.exports = [
   ...neostandard({
     ts: true,
     ignores: [
       ...neostandard.resolveIgnoresFromGitignore(),
+      '**/*/*.cjs',
     ],
     semi: true,
   }),
@@ -13,7 +14,6 @@ module.exports = [
       strict: ['error', 'global'],
       'no-loop-func': ['error'],
       curly: ['error', 'multi-line', 'consistent'],
-      'consistent-return': ['error', { treatUndefinedAsUnspecified: true }],
       'no-unused-private-class-members': ['error'],
       'no-invalid-this': ['error'],
       'class-methods-use-this': ['warn'],
@@ -26,15 +26,42 @@ module.exports = [
       'no-console': ['off'],
       'max-nested-callbacks': [
         'error',
-        {
-          max: 5,
-        },
+        { max: 5 },
       ],
       'no-lonely-if': ['error'],
       'no-nested-ternary': ['error'],
       'object-shorthand': ['error'],
       'operator-assignment': ['error', 'always'],
+      'n/handle-callback-err': ['off'],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/jsx-closing-tag-location': ['error', 'line-aligned'],
+      '@stylistic/jsx-self-closing-comp': ['error', {
+        component: true,
+        html: true,
+      }],
+      '@stylistic/jsx-wrap-multilines': ['error', {
+        declaration: 'parens',
+        assignment: 'parens',
+        return: 'parens',
+        arrow: 'parens',
+        condition: 'ignore',
+        logical: 'ignore',
+        prop: 'ignore',
+        propertyValue: 'ignore',
+      }],
+      '@stylistic/max-len': [
+        'error',
+        {
+          code: 80,
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+      '@stylistic/multiline-ternary': 'off',
       '@stylistic/space-before-function-paren': [
         'error',
         {
