a89749393a7c8b131612c868964f8bef26599731
Ross Kelly <5331533+rssk@users.noreply.github.com>
2025-10-22 02:30:09 -0400

    modify the env to not interfer across services

--- fileServer/src/config.ts
+++ fileServer/src/config.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const AUTHENTICATION_URL = process.env.AUTHENTICATION_URL || ''
-export const BASE_DIR = process.env.BASE_DIR || ''
-export const PORT = Number(process.env.PORT || '')
+export const BASE_DIR = process.env.FILE_SERVER_BASE_DIR || ''
+export const PORT = Number(process.env.FILE_SERVER_PORT || '')
 export const LOG_PATH = process.env.LOG_PATH || ''
